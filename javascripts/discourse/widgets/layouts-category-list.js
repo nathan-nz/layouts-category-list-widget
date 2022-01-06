0d285429a2476a58a61b556c1181d0b24bd5b2aa
Nathan Kershaw <63566309+nathan-nz@users.noreply.github.com>
2022-01-06 13:08:09 +1300

    Update layouts-category-list.js

--- javascripts/discourse/widgets/layouts-category-list.js
+++ javascripts/discourse/widgets/layouts-category-list.js
@@ -537,12 +537,12 @@ createWidget('layouts-category-link', {
     );
 
 // Code to add globes to widget 
-    if (category.name === "Open Forum") {
-      contents.push(iconNode("globe", { class: 'category-lock-icon' }));
-    }
-    if (category.name === "Blog") {
-      contents.push(iconNode("globe", { class: 'category-lock-icon' }));
-    }
+//    if (category.name === "Open Forum") {
+//      contents.push(iconNode("globe", { class: 'category-lock-icon' }));
+//    }
+//    if (category.name === "Blog") {
+//      contents.push(iconNode("globe", { class: 'category-lock-icon' }));
+//    }
     
     
     if (category.read_restricted) {
